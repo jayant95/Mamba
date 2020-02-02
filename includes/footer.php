0d02036e0a20b0e3ec0c3bcc9f61e78e7e619e38
jayant95 <jht1995@hotmail.com>
2020-02-02 12:57:22 -0800

    Add instagram link to footer
    Linked MambaAndMe instagram page

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,6 @@
 
 		<div class="footer">
+			<a href="https://www.instagram.com/mambaandme24/">Instagram</a>
 			<p>© 2020 MambaAndMe. All Rights Reserved.</p>
 		</div>
 	</body>
--- index.php
+++ index.php
@@ -255,6 +255,7 @@
 </div>
 
 <div class="footer">
+    <a href="https://www.instagram.com/mambaandme24/">Instagram</a>
     <p>© 2020 MambaAndMe. All Rights Reserved.</p>
 </div>
 <script src="js/hearts.js"></script>
